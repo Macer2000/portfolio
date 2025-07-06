ebb3664cd868357c9e7a1fbb444e8fcfe497356f
Macer2000-coder <macermac2017@gmail.com>
2025-07-06 18:32:36 +0530

    Updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -247,7 +247,7 @@ document.addEventListener('DOMContentLoaded', function () {
     window.addEventListener('click', function (e) {
       if (e.target === popup) {
         popup.classList.remove('show');
-      }
+    }
     });
   }
 });
